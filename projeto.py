245b629575a50d9ba0e0229c36f07fc761e494b5
diogoserrao <diogorabimserrao@gmail.com>
2023-12-05 12:06:07 +0000

    load dos users

--- projeto.py
+++ projeto.py
@@ -1,16 +1,11 @@
-import json
+from funcoes import load
 
 
-informacao = int(input("Administrador pressione 1\nCliente pressione 2\nnº:"))
 
-def autenticacao(informacao):
-    if informacao == 1:
-        email = input("Introduza o seu email:")
-        senha = input("Introduza a sua senha:")
-    elif informacao == 2:
-        email2= input("Introduza o seu email:")
 
-    return informacao
 
-print(autenticacao(informacao))
 
+listUsers = load( "users.json")
+print(listUsers)
+
+user = login(listUsers)
