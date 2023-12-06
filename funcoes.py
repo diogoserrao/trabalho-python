cd2045fadc19d22c265ed57dfe7f27783da46c67
diogoserrao <diogorabimserrao@gmail.com>
2023-12-06 15:28:32 +0000

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -12,4 +12,22 @@ def load(filename):
     else:
         return data
     
+def make_reservaton(user):
 
+def see_historic(user):
+    
+def create_menu(user):
+    while True:
+        print("\nMarcar Refeição")
+        print("Ver Historico")
+        print("Sair")
+
+        opcao = input("Escolha uma opção")
+        if opcao == 1:
+            print(make_reservaton)
+        elif opcao == 2:
+            print(see_historic)
+        elif opcao == 3:
+            
+        else:
+            print("Opção invalida")    
