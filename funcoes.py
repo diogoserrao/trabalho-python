d1e14c76a9829895a2fe704ff8c9c1c8449f8e72
diogoserrao <diogorabimserrao@gmail.com>
2023-12-09 15:07:32 +0000

    inicio da função da reserva

--- funcoes.py
+++ funcoes.py
@@ -13,8 +13,24 @@ def load(filename):
         return data
     
 def make_reservaton(user):
+    data = input("Introduza a data no formato aaaa-mm-dd: ")
+    hora = input("Horário previsto: ")
+    dia = input("Introduza a hora prevista de chegada no formato hh:mm: ")
+    #se não estiver sobreposta pergunta quantas pessoas são
+    if True :
+        pessoas = input("Indique o numero de pessoas (max 12): ")
+        criancas = input("INdique o numero de crianças: ")
+   
 
 def see_historic(user):
+    try:
+        with open("historico.json", 'r') as f:
+            data =json.historico(f)
+    except:
+        print("\nErro: não foi possível abrir o ficheiro")
+        return[]
+    else:
+        return data
     
 def create_menu(user):
     while True:
@@ -28,6 +44,6 @@ def create_menu(user):
         elif opcao == 2:
             print(see_historic)
         elif opcao == 3:
-            
+            break
         else:
             print("Opção invalida")    
